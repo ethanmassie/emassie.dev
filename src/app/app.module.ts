1e53ce23f29205538556215c3cc99fb7fe496993
Ethan Massie <ethan.massie1@gmail.com>
2020-01-25 11:40:36 -0600

    added app routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { NgModule } from '@angular/core';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeModule } from './home/home.module';
+import { ProjectsModule } from './projects/projects.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      NavbarComponent
+   ],
+   imports: [
+      BrowserModule,
+      BrowserAnimationsModule,
+      MatMenuModule,
+      MatIconModule,
+
+      HomeModule,
+      ProjectsModule,
+      AppRoutingModule,
+   ],
+   providers: [
+     MatIconRegistry
+   ],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
