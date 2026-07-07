1de751670051430f4764a0992d7759e4f911baa0
Ethan Massie <ethan@emassie.dev>
2026-07-06 20:35:00 -0500

    improve cat animation

--- src/pages/home/scripts/cat-carousel-click.ts
+++ src/pages/home/scripts/cat-carousel-click.ts
@@ -1,3 +1,6 @@
+const ANIMATION_MS = 500;
+const DELAY_MS = 50;
+
 (() => {
   const html = document.querySelector('html');
   if (!html || !(html instanceof Element)) {
@@ -21,22 +24,48 @@
     clone.style.left = pxStr(rect.left);
     clone.style.height = pxStr(rect.height);
     clone.style.width = pxStr(rect.width);
-    // delay style changes to ensure transitions occur
-    setTimeout(() => {
-      clone.classList.add('cat-img-clone');
-    }, 50);
+    clone.style.transition = `all ease-in-out ${ANIMATION_MS}ms`;
 
     const backdrop = document.createElement('div');
     backdrop.classList.add('cat-img-backdrop');
-    backdrop.addEventListener('click', (e) => {
+
+    const cleanup = (e: Event) => {
       e.preventDefault();
       e.stopPropagation();
-      clone.remove();
+      html.removeEventListener('keydown', cleanupOnEsc);
       backdrop.remove();
-      document.body.inert = false;
-    });
+      clone.classList.remove('cat-img-clone');
+      // wait for animation to finish
+      setTimeout(() => {
+        clone.remove();
+        document.body.inert = false;
+      }, ANIMATION_MS);
+    };
+    backdrop.addEventListener('click', cleanup);
 
-    document.querySelector('html')?.append(clone, backdrop);
+    const cleanupOnEsc = (e: KeyboardEvent) => {
+      if (e.key !== 'Escape') {
+        return;
+      }
+
+      cleanup(e);
+    };
+
+    html.append(clone, backdrop);
+    html.addEventListener('keydown', cleanupOnEsc);
     document.body.inert = true;
+
+    // delay style changes to ensure transitions occur
+    setTimeout(() => {
+      clone.classList.add('cat-img-clone');
+    }, DELAY_MS);
+
+    // determine what the auto width was calculated and set it as a custom property so the transition out can animate the width shrinking
+    setTimeout(
+      () => {
+        clone.style.setProperty('--_auto-width', pxStr(clone.clientWidth));
+      },
+      ANIMATION_MS + DELAY_MS + DELAY_MS,
+    );
   });
 })();
