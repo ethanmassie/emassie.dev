f1b6632c50250db0687b30d6e785d7d9a239fa99
Ethan Massie <ethan@emassie.dev>
2026-08-29 12:09:41 -0500

    chore: improve handlebars context

--- vite.config.js
+++ vite.config.js
@@ -1,50 +1,65 @@
-import { resolve } from 'path';
+import { resolve } from 'node:path';
 import handlebars from 'vite-plugin-handlebars';
-import links from './links.json';
 import { title } from 'process';
-import { readdirSync } from 'node:fs';
+import { existsSync, readdirSync, readFileSync, statSync } from 'node:fs';
 import findPages from './find-pages';
-import { mdiMusicNote, mdiGuitarAcoustic } from '@mdi/js';
+import * as icons from '@mdi/js';
+import { exit } from 'node:process';
 
 const catFiles = readdirSync(resolve(import.meta.dirname, 'public', 'cats'));
 
+/**
+ * Map of context necessary for specific html pages. Only needs to be used if data cannot be stored in the relative context.json file
+ */
 const contextMap = {
   '/index.html': {
-    links,
     catFiles,
   },
-  '/music/index.html': {
-    secondaryNavLabel: 'Music',
-    secondaryNavLinks: [
-      {
-        name: 'listens',
-        label: 'Listens',
-        icon: mdiMusicNote,
-      },
-      {
-        name: 'classical-guitar',
-        label: 'Classical Guitar',
-        icon: mdiGuitarAcoustic,
-      },
-    ],
-  },
-  '/projects/index.html': {
-    secondaryNavLabel: 'Project',
-    secondaryNavLinks: [
-      {
-        name: 'deskcycle-kb',
-        label: 'Deskcycle KB',
-      },
-    ],
-  },
 };
+
+/**
+ *
+ * @param {string} path
+ * @returns {any}
+ */
+function retrieveContext(path) {
+  const baseContext = contextMap[path] || {};
+  const contextPath = resolve(
+    import.meta.dirname,
+    ...path.split('/'),
+    '..',
+    'context.json',
+  );
+
+  if (existsSync(contextPath)) {
+    try {
+      const data = readFileSync(contextPath).toString();
+      const jsonData = JSON.parse(data);
+      return { ...jsonData, ...baseContext };
+    } catch (e) {
+      console.error(`Failed to read: ${contextPath}`);
+      console.error(e);
+      exit(1);
+    }
+  }
+
+  return baseContext;
+}
 
 export default {
   plugins: [
     handlebars({
       partialDirectory: resolve(import.meta.dirname, 'src', 'partials'),
+      helpers: {
+        json: (ctx) => {
+          return JSON.stringify(ctx);
+        },
+        mdiIcon: (ctx) => {
+          return icons[ctx] || '';
+        },
+      },
       context(path) {
-        const pageContext = contextMap[path] || {};
+        const pageContext = retrieveContext(path);
         return pageContext;
       },
     }),
